16f63e73db4f826aa58c55d2dafdf4350cf7fb37
Macrina <macrina.gomis@gmail.com>
2025-06-12 01:33:29 +0530

    Restaurant class

--- FoodDeliverySystem/Program.cs
+++ FoodDeliverySystem/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FoodDeliverySystem.services;
 
 namespace FoodDeliverySystem
 {
@@ -10,6 +11,22 @@ namespace FoodDeliverySystem
     {
         static void Main(string[] args)
         {
+            RestaurantService rs = new RestaurantService();
+
+            rs.AddRestaurant("Burger Shack", "Main Street");
+            rs.AddRestaurant("Pizza Palace", "Ocean Road");
+
+            rs.ViewRestaurants();
+
+            rs.UpdateRestaurant(1, "Burger Heaven", "Sunset Ave");
+
+            rs.ViewRestaurants();
+
+            rs.DeleteRestaurant(2);
+
+            rs.ViewRestaurants();
+
+            Console.ReadLine(); // Keep console open
         }
     }
 }
--- FoodDeliverySystem/modules/Restaurant.cs
+++ FoodDeliverySystem/modules/Restaurant.cs
@@ -8,5 +8,49 @@ namespace FoodDeliverySystem.modules
 {
     class Restaurant
     {
+        private int id;
+        private string name;
+        private string address;
+
+        // Constructor
+        public Restaurant(int id, string name, string address)
+        {
+            SetId(id);
+            SetName(name);
+            SetAddress(address);
+        }
+
+        // ID Getter & Setter
+        public int GetId()
+        {
+            return id;
+        }
+
+        public void SetId(int value)
+        {
+            id = value;
+        }
+
+        // Name Getter & Setter
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetName(string value)
+        {
+            name = value;
+        }
+
+        // Address Getter & Setter
+        public string GetAddress()
+        {
+            return address;
+        }
+
+        public void SetAddress(string value)
+        {
+            address = value;
+        }
     }
 }
