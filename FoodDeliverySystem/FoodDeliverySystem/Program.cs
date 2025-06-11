765602c4be91f24c3e6e0a0b8a767ff2a6e0dc3e
Macrina <macrina.gomis@gmail.com>
2025-06-11 23:39:22 +0530

    structure

--- FoodDeliverySystem/FoodDeliverySystem/Program.cs
+++ FoodDeliverySystem/FoodDeliverySystem/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace FoodDeliverySystem
 {
-    class Delivery
+    class Program
     {
+        static void Main(string[] args)
+        {
+        }
     }
 }
--- FoodDeliverySystem/FoodDeliverySystem/Properties/AssemblyInfo.cs
+++ FoodDeliverySystem/FoodDeliverySystem/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("FoodDeliverySystem")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("FoodDeliverySystem")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("abd1e875-4fab-47d4-a849-f5c7495d2edf")]
+[assembly: Guid("214212fd-d35d-480e-92e4-82b24af4d5a5")]
 
 // Version information for an assembly consists of the following four values:
 //
