e2a120100ac47c150d53d9ddd8fa3753cec75577
Amanda <ajayasinghe045@cicracampus.net>
2025-06-14 09:38:46 +0530

    Admin register form updated

--- FoodDeliverySystem/Program.cs
+++ FoodDeliverySystem/Program.cs
@@ -15,29 +15,10 @@ namespace FoodDeliverySystem
     {
         static void Main(string[] args)
         {
-            // Setup dummy customer and restaurant
-            Customer customer = new Customer(1, "John Doe", "123 Street", "0781234567");
-            Restaurant restaurant = new Restaurant(1, "Pizza Palace", "Downtown");
-
-            // Dummy menu items
-            MenuItem item1 = new MenuItem(1, "Cheese Pizza", 1500.0, true);
-            MenuItem item2 = new MenuItem(2, "Veg Pizza", 1300.0, true);
-            List<MenuItem> orderedItems = new List<MenuItem> { item1, item2 };
-
-            // Create the OrderService
-            OrderService orderService = new OrderService();
-
-            // Place an order
-            orderService.PlaceOrder(customer, restaurant, orderedItems);
-
-            // View all orders
-            orderService.ViewOrders();
-
-            Console.ReadLine(); // Keep the console window open
-
+            Application.EnableVisualStyles();   // Makes the app look modern
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Startform());
         }
     }
 
 }
-
-
--- FoodDeliverySystem/views/AdminRegisterForm.Designer.cs
+++ FoodDeliverySystem/views/AdminRegisterForm.Designer.cs
@@ -42,75 +42,84 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(219, 39);
+            this.label1.Location = new System.Drawing.Point(164, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(338, 40);
+            this.label1.Size = new System.Drawing.Size(272, 31);
             this.label1.TabIndex = 0;
             this.label1.Text = "Admin Register Form";
             // 
             // lblAdminName
             // 
             this.lblAdminName.AutoSize = true;
-            this.lblAdminName.Location = new System.Drawing.Point(199, 133);
+            this.lblAdminName.Location = new System.Drawing.Point(149, 108);
+            this.lblAdminName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblAdminName.Name = "lblAdminName";
-            this.lblAdminName.Size = new System.Drawing.Size(85, 16);
+            this.lblAdminName.Size = new System.Drawing.Size(67, 13);
             this.lblAdminName.TabIndex = 1;
             this.lblAdminName.Text = "Admin Name";
             // 
             // lblAdminPassword
             // 
             this.lblAdminPassword.AutoSize = true;
-            this.lblAdminPassword.Location = new System.Drawing.Point(176, 200);
+            this.lblAdminPassword.Location = new System.Drawing.Point(132, 162);
+            this.lblAdminPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblAdminPassword.Name = "lblAdminPassword";
-            this.lblAdminPassword.Size = new System.Drawing.Size(108, 16);
+            this.lblAdminPassword.Size = new System.Drawing.Size(85, 13);
             this.lblAdminPassword.TabIndex = 2;
             this.lblAdminPassword.Text = "Admin Password";
             // 
             // lblConfirmpassword
             // 
             this.lblConfirmpassword.AutoSize = true;
-            this.lblConfirmpassword.Location = new System.Drawing.Point(176, 266);
+            this.lblConfirmpassword.Location = new System.Drawing.Point(132, 216);
+            this.lblConfirmpassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblConfirmpassword.Name = "lblConfirmpassword";
-            this.lblConfirmpassword.Size = new System.Drawing.Size(114, 16);
+            this.lblConfirmpassword.Size = new System.Drawing.Size(90, 13);
             this.lblConfirmpassword.TabIndex = 3;
             this.lblConfirmpassword.Text = "Confirm password";
             // 
             // txtAdminName
             // 
-            this.txtAdminName.Location = new System.Drawing.Point(296, 127);
+            this.txtAdminName.Location = new System.Drawing.Point(222, 103);
+            this.txtAdminName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtAdminName.Name = "txtAdminName";
-            this.txtAdminName.Size = new System.Drawing.Size(269, 22);
+            this.txtAdminName.Size = new System.Drawing.Size(203, 20);
             this.txtAdminName.TabIndex = 4;
             // 
             // txtAdminPassword
             // 
-            this.txtAdminPassword.Location = new System.Drawing.Point(296, 200);
+            this.txtAdminPassword.Location = new System.Drawing.Point(222, 162);
+            this.txtAdminPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtAdminPassword.Name = "txtAdminPassword";
-            this.txtAdminPassword.Size = new System.Drawing.Size(269, 22);
+            this.txtAdminPassword.Size = new System.Drawing.Size(203, 20);
             this.txtAdminPassword.TabIndex = 5;
             // 
             // txtConfirmpassword
             // 
-            this.txtConfirmpassword.Location = new System.Drawing.Point(296, 263);
+            this.txtConfirmpassword.Location = new System.Drawing.Point(222, 214);
+            this.txtConfirmpassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtConfirmpassword.Name = "txtConfirmpassword";
-            this.txtConfirmpassword.Size = new System.Drawing.Size(269, 22);
+            this.txtConfirmpassword.Size = new System.Drawing.Size(203, 20);
             this.txtConfirmpassword.TabIndex = 6;
             // 
             // btnRegister
             // 
             this.btnRegister.BackColor = System.Drawing.SystemColors.Info;
-            this.btnRegister.Location = new System.Drawing.Point(296, 333);
+            this.btnRegister.Location = new System.Drawing.Point(222, 271);
+            this.btnRegister.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnRegister.Name = "btnRegister";
-            this.btnRegister.Size = new System.Drawing.Size(85, 35);
+            this.btnRegister.Size = new System.Drawing.Size(64, 28);
             this.btnRegister.TabIndex = 7;
             this.btnRegister.Text = "Register";
             this.btnRegister.UseVisualStyleBackColor = false;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
             // 
             // AdminRegisterForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.btnRegister);
             this.Controls.Add(this.txtConfirmpassword);
             this.Controls.Add(this.txtAdminPassword);
@@ -119,6 +128,7 @@
             this.Controls.Add(this.lblAdminPassword);
             this.Controls.Add(this.lblAdminName);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "AdminRegisterForm";
             this.Text = "AdminRegisterForm";
             this.ResumeLayout(false);
