4c09ec0d091e601ac344c7cca7803ac0c347ba45
Sakithma <LENOVO@SAKI>
2025-06-14 13:31:21 +0530

    viewmenu

--- FoodDeliverySystem/views/Managerestaurantsform.Designer.cs
+++ FoodDeliverySystem/views/Managerestaurantsform.Designer.cs
@@ -31,11 +31,23 @@
             this.lblmanagerestaurant = new System.Windows.Forms.Label();
             this.lblAddaRestaurant = new System.Windows.Forms.Label();
             this.dgvgrid = new System.Windows.Forms.DataGridView();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblRestaurantName = new System.Windows.Forms.Label();
+            this.txtRestaurantName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.lblname = new System.Windows.Forms.Label();
+            this.lbladdress1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvgrid)).BeginInit();
             this.SuspendLayout();
             // 
@@ -70,59 +82,177 @@
             this.dgvgrid.Size = new System.Drawing.Size(527, 184);
             this.dgvgrid.TabIndex = 2;
             // 
-            // label2
+            // lblAddress
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(182, 432);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 16);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "label2";
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(175, 435);
+            this.lblAddress.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(58, 16);
+            this.lblAddress.TabIndex = 4;
+            this.lblAddress.Text = "Address";
             // 
-            // label1
+            // lblRestaurantName
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(182, 387);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "label1";
+            this.lblRestaurantName.AutoSize = true;
+            this.lblRestaurantName.Location = new System.Drawing.Point(150, 390);
+            this.lblRestaurantName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblRestaurantName.Name = "lblRestaurantName";
+            this.lblRestaurantName.Size = new System.Drawing.Size(112, 16);
+            this.lblRestaurantName.TabIndex = 5;
+            this.lblRestaurantName.Text = "Restaurant Name";
             // 
-            // textBox1
+            // txtRestaurantName
             // 
-            this.textBox1.Location = new System.Drawing.Point(267, 384);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(289, 22);
-            this.textBox1.TabIndex = 6;
+            this.txtRestaurantName.Location = new System.Drawing.Point(267, 384);
+            this.txtRestaurantName.Name = "txtRestaurantName";
+            this.txtRestaurantName.Size = new System.Drawing.Size(289, 22);
+            this.txtRestaurantName.TabIndex = 6;
             // 
-            // textBox2
+            // txtAddress
             // 
-            this.textBox2.Location = new System.Drawing.Point(267, 432);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(289, 22);
-            this.textBox2.TabIndex = 7;
+            this.txtAddress.Location = new System.Drawing.Point(268, 432);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(289, 22);
+            this.txtAddress.TabIndex = 7;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(369, 474);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(91, 41);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(368, 474);
+            this.button1.Location = new System.Drawing.Point(369, 712);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(91, 41);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "button1";
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Add";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(268, 670);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(289, 22);
+            this.textBox1.TabIndex = 13;
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(267, 622);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(289, 22);
+            this.txtname.TabIndex = 12;
+            // 
+            // lblname
+            // 
+            this.lblname.AutoSize = true;
+            this.lblname.Location = new System.Drawing.Point(175, 628);
+            this.lblname.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblname.Name = "lblname";
+            this.lblname.Size = new System.Drawing.Size(44, 16);
+            this.lblname.TabIndex = 11;
+            this.lblname.Text = "Name";
+            // 
+            // lbladdress1
+            // 
+            this.lbladdress1.AutoSize = true;
+            this.lbladdress1.Location = new System.Drawing.Point(175, 673);
+            this.lbladdress1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbladdress1.Name = "lbladdress1";
+            this.lbladdress1.Size = new System.Drawing.Size(58, 16);
+            this.lbladdress1.TabIndex = 10;
+            this.lbladdress1.Text = "Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(343, 565);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 24);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Add a Restaurant";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(369, 949);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 41);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(268, 907);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(289, 22);
+            this.textBox3.TabIndex = 19;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(267, 859);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(289, 22);
+            this.textBox4.TabIndex = 18;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(150, 865);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 16);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Restaurant Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(175, 910);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 16);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Address";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(343, 802);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(158, 24);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Add a Restaurant";
             // 
             // Managerestaurantsform
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 917);
+            this.ClientSize = new System.Drawing.Size(800, 1028);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.lblname);
+            this.Controls.Add(this.lbladdress1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtRestaurantName);
+            this.Controls.Add(this.lblRestaurantName);
+            this.Controls.Add(this.lblAddress);
             this.Controls.Add(this.dgvgrid);
             this.Controls.Add(this.lblAddaRestaurant);
             this.Controls.Add(this.lblmanagerestaurant);
@@ -141,10 +271,22 @@
         private System.Windows.Forms.Label lblmanagerestaurant;
         private System.Windows.Forms.Label lblAddaRestaurant;
         private System.Windows.Forms.DataGridView dgvgrid;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label lblRestaurantName;
+        private System.Windows.Forms.TextBox txtRestaurantName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.Label lblname;
+        private System.Windows.Forms.Label lbladdress1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
